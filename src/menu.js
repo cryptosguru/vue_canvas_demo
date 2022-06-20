65d614f77a9537d535de2a060b02418f9290b4c7
David <jackaldream88@gmail.com>
2022-06-20 19:58:00 +0900

    update UI

--- src/menu.js
+++ src/menu.js
@@ -1,15 +1,8 @@
 import {
   mdiAccountCircle,
   mdiDesktopMac,
-  mdiGithub,
-  mdiAlertCircle,
-  mdiMonitorShimmer,
   mdiSquareEditOutline,
-  mdiTable,
-  mdiViewList,
-  mdiTelevisionGuide,
-  mdiResponsive,
-  mdiPalette
+  mdiTable
 } from '@mdi/js'
 
 export default [
@@ -34,57 +27,9 @@ export default [
       icon: mdiSquareEditOutline
     },
     {
-      to: '/ui',
-      label: 'UI',
-      icon: mdiTelevisionGuide
-    },
-    {
-      to: '/responsive',
-      label: 'Responsive',
-      icon: mdiResponsive
-    },
-    {
-      to: '/',
-      label: 'Styles',
-      icon: mdiPalette
-    },
-    {
       to: '/profile',
       label: 'Profile',
       icon: mdiAccountCircle
-    },
-    {
-      to: '/error',
-      label: 'Error',
-      icon: mdiAlertCircle
-    },
-    {
-      label: 'Submenus',
-      subLabel: 'Submenus Example',
-      icon: mdiViewList,
-      menu: [
-        {
-          label: 'Sub-item One'
-        },
-        {
-          label: 'Sub-item Two'
-        }
-      ]
-    }
-  ],
-  'About',
-  [
-    {
-      href: 'https://justboil.me/tailwind-admin-templates/vue-dashboard/',
-      label: 'Premium version',
-      icon: mdiMonitorShimmer,
-      target: '_blank'
-    },
-    {
-      href: 'https://github.com/jackalstar88/vue_canvas_demo',
-      label: 'GitHub',
-      icon: mdiGithub,
-      target: '_blank'
     }
   ]
 ]
